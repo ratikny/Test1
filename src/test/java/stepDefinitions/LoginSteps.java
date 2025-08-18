d1272d8e2cb41572f12f81b5330bb8b4dddac69a
ratikny <90168570+ratikny@users.noreply.github.com>
2025-08-18 17:27:36 -0400

    Run Chrome in headless mode for Jenkins

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -21,6 +22,94 @@ public class LoginSteps {
     //WebDriver driver = new ChromeDriver();
     private WebDriver driver;
     private WebDriverWait wait;
+
+    @Given("I opened main page of website")
+    public void i_opened_main_page_of_website() {
+        WebDriverManager.chromedriver().setup();
+
+        // --- НАЧАЛО ИЗМЕНЕНИЙ ---
+
+        // Создаём опции для Chrome
+        ChromeOptions options = new ChromeOptions();
+
+        // Включаем headless-режим. Браузер будет работать в фоне.
+        options.addArguments("--headless");
+
+        // Эти опции часто нужны для стабильной работы в Docker/Linux
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080"); // Задаём размер окна
+
+        // Создаём драйвер с новыми опциями
+        driver = new ChromeDriver(options);
+
+        // --- КОНЕЦ ИЗМЕНЕНИЙ ---
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+
+        // driver.manage().window().maximize(); // ЭТА СТРОКА УДАЛЕНА
+
+        driver.get("https://demoqa.com/");
+
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//img[@class='banner-image']")
+        ));
+
+        System.out.println("Stage 1");
+    }
+
+    @When("I click {string} button")
+    public void i_click_button(String name) {
+        // Скрываем баннер, если есть
+        try {
+            WebElement banner = driver.findElement(By.id("fixedban"));
+            ((JavascriptExecutor) driver).executeScript("arguments[0].style.display='none'", banner);
+            System.out.println("✅ Баннер скрыт.");
+        } catch (Exception e) {
+            System.out.println("⚠️ Баннер не найден, пропускаем.");
+        }
+
+        // Кликаем по карточке (например, Elements)
+        WebElement elementsCard = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//h5[text()='" + name + "']/ancestor::div[@class='card mt-4 top-card']")));
+        elementsCard.click();
+        System.out.println("✅ Клик по '" + name + "'");
+    }
+
+    @And("I click {string} section")
+    public void i_click_section(String sectionName) {
+        WebElement menuItem = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//span[text()='" + sectionName + "']")));
+        menuItem.click();
+        System.out.println("✅ Клик по разделу '" + sectionName + "'");
+    }
+
+    @And("I choose {string} radio button")
+    public void i_choose_radio_button(String option) {
+        WebElement radio = driver.findElement(By.id("yesRadio"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", radio);
+        System.out.println("✅ Выбран radio: " + option);
+    }
+
+    @And("I fill {string} text box")
+    public void i_fill_text_box(String fieldName) {
+        if (fieldName.equalsIgnoreCase("Full Name")) {
+            driver.findElement(By.id("userName")).sendKeys("Jordan Brown");
+            System.out.println("✅ Ввёл Full Name");
+        } else if (fieldName.equalsIgnoreCase("Email")) {
+            driver.findElement(By.id("userEmail")).sendKeys("example@mail.com");
+            System.out.println("✅ Ввёл Email");
+        }
+    }
+
+    @Then("I close browser")
+    public void i_close_browser() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
+
+
 
 
 
@@ -112,71 +201,71 @@ public class LoginSteps {
 //       driver.quit();
 //    }
 
-    @Given("I opened main page of website")
-    public void i_opened_main_page_of_website() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
-
-        driver.manage().window().maximize();
-        driver.get("https://demoqa.com/");
-
-        wait.until(ExpectedConditions.presenceOfElementLocated(
-                By.xpath("//img[@class='banner-image']")
-        ));
-
-        System.out.println("Stage 1");
-    }
-
-    @When("I click {string} button")
-    public void i_click_button(String name) {
-        // Скрываем баннер, если есть
-        try {
-            WebElement banner = driver.findElement(By.id("fixedban"));
-            ((JavascriptExecutor) driver).executeScript("arguments[0].style.display='none'", banner);
-            System.out.println("✅ Баннер скрыт.");
-        } catch (Exception e) {
-            System.out.println("⚠️ Баннер не найден, пропускаем.");
-        }
-
-        // Кликаем по карточке (например, Elements)
-        WebElement elementsCard = wait.until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//h5[text()='" + name + "']/ancestor::div[@class='card mt-4 top-card']")));
-        elementsCard.click();
-        System.out.println("✅ Клик по '" + name + "'");
-    }
-
-    @And("I click {string} section")
-    public void i_click_section(String sectionName) {
-        WebElement menuItem = wait.until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//span[text()='" + sectionName + "']")));
-        menuItem.click();
-        System.out.println("✅ Клик по разделу '" + sectionName + "'");
-    }
-
-    @And("I choose {string} radio button")
-    public void i_choose_radio_button(String option) {
-        WebElement radio = driver.findElement(By.id("yesRadio"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", radio);
-        System.out.println("✅ Выбран radio: " + option);
-    }
-
-    @And("I fill {string} text box")
-    public void i_fill_text_box(String fieldName) {
-        if (fieldName.equalsIgnoreCase("Full Name")) {
-            driver.findElement(By.id("userName")).sendKeys("Jordan Brown");
-            System.out.println("✅ Ввёл Full Name");
-        } else if (fieldName.equalsIgnoreCase("Email")) {
-            driver.findElement(By.id("userEmail")).sendKeys("example@mail.com");
-            System.out.println("✅ Ввёл Email");
-        }
-    }
-
-    @Then("I close browser")
-    public void i_close_browser() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.quit();
-    }
-}
-
-
+//    @Given("I opened main page of website")
+//    public void i_opened_main_page_of_website() {
+//        WebDriverManager.chromedriver().setup();
+//        driver = new ChromeDriver();
+//        wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+//
+//        driver.manage().window().maximize();
+//        driver.get("https://demoqa.com/");
+//
+//        wait.until(ExpectedConditions.presenceOfElementLocated(
+//                By.xpath("//img[@class='banner-image']")
+//        ));
+//
+//        System.out.println("Stage 1");
+//    }
+//
+//    @When("I click {string} button")
+//    public void i_click_button(String name) {
+//        // Скрываем баннер, если есть
+//        try {
+//            WebElement banner = driver.findElement(By.id("fixedban"));
+//            ((JavascriptExecutor) driver).executeScript("arguments[0].style.display='none'", banner);
+//            System.out.println("✅ Баннер скрыт.");
+//        } catch (Exception e) {
+//            System.out.println("⚠️ Баннер не найден, пропускаем.");
+//        }
+//
+//        // Кликаем по карточке (например, Elements)
+//        WebElement elementsCard = wait.until(ExpectedConditions.elementToBeClickable(
+//                By.xpath("//h5[text()='" + name + "']/ancestor::div[@class='card mt-4 top-card']")));
+//        elementsCard.click();
+//        System.out.println("✅ Клик по '" + name + "'");
+//    }
+//
+//    @And("I click {string} section")
+//    public void i_click_section(String sectionName) {
+//        WebElement menuItem = wait.until(ExpectedConditions.elementToBeClickable(
+//                By.xpath("//span[text()='" + sectionName + "']")));
+//        menuItem.click();
+//        System.out.println("✅ Клик по разделу '" + sectionName + "'");
+//    }
+//
+//    @And("I choose {string} radio button")
+//    public void i_choose_radio_button(String option) {
+//        WebElement radio = driver.findElement(By.id("yesRadio"));
+//        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", radio);
+//        System.out.println("✅ Выбран radio: " + option);
+//    }
+//
+//    @And("I fill {string} text box")
+//    public void i_fill_text_box(String fieldName) {
+//        if (fieldName.equalsIgnoreCase("Full Name")) {
+//            driver.findElement(By.id("userName")).sendKeys("Jordan Brown");
+//            System.out.println("✅ Ввёл Full Name");
+//        } else if (fieldName.equalsIgnoreCase("Email")) {
+//            driver.findElement(By.id("userEmail")).sendKeys("example@mail.com");
+//            System.out.println("✅ Ввёл Email");
+//        }
+//    }
+//
+//    @Then("I close browser")
+//    public void i_close_browser() throws InterruptedException {
+//        Thread.sleep(2000);
+//        driver.quit();
+//    }
+//}
+//
+//
